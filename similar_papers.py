9dbc53368c40f2bbe65933c7aa3cf13c5cc7e740
Tomasz Siudalski <tomas@LAPTOP-KNVNQ5KP>
2023-11-29 23:57:44 +0100

    Bugfix

--- similar_papers.py
+++ similar_papers.py
@@ -30,7 +30,13 @@ def extract_title_from_graph(graph: Graph):
 
 def main():
 
-    connections.connect("default", host="localhost", port="19530")
+    connections.connect(
+        alias='default',
+        user='username',
+        password='password',
+        host='milvus-standalone',
+        port='19530'
+    )
     
     database_name = "papers"
     collection_name = "research_papers"
